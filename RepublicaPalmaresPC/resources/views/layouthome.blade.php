8c425e49f592a54e1eee42606df473bdd8564e8f
Ney Rocha <neyrocha.brasil@gmail.com>
2019-10-13 17:48:26 -0300

    ...

--- RepublicaPalmaresPC/resources/views/layouthome.blade.php
+++ RepublicaPalmaresPC/resources/views/layouthome.blade.php
@@ -116,13 +116,10 @@
                     <!-- botão Sair do sitema -->
                     <ul class="nav navbar-top-links navbar-right">
                         <li>
-
-
-                                    <a href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                                     <i class="fa fa-sign-out" aria-hidden="true"></i>Sair
-                                    </a>
+                            <a href="{{ route('logout') }}" onclick="event.preventDefault();
+                                    document.getElementById('logout-form').submit();">
+                                <i class="fa fa-sign-out" aria-hidden="true"></i>Sair
+                            </a>
 
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                 @csrf
