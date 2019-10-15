f0246881738a1bc80970c1bb5bd759b944a3227c
Samuel <samuelsoares177778@gmail.com>
2019-10-15 18:55:03 -0300

    cadastro pessoa ok

--- RepublicaPalmaresPC/app/Telefone.php
+++ RepublicaPalmaresPC/app/Telefone.php
@@ -8,9 +8,12 @@ class Telefone extends Model
 {   
     protected $table = 'telefone';
     public $timestamps = false;
+    protected $fillable = [
+        'telefone','telefone2'
+    ]; //salvar atributso em massa no metodo create
 
     public function pessoa()
     {
-        return $this->belongsTo(Pessoa::class);
+        return $this->belongsTo(Pessoa::class,'id_telefone');
     }
 }
