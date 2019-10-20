6b897335b0ea1b072648b1fc677d6175bc33eaaa
Samuel <samuelsoares177778@gmail.com>
2019-10-20 13:56:12 -0300

    arrumando relacionamento many-to-many

---
+++ RepublicaPalmaresPC/app/ColaboradorCargo.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ColaboradorCargo extends Model
+{   
+    protected $table = 'colaborador_cargo';
+    public $timestamps = false;
+}
